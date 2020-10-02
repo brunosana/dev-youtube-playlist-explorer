867fd314b5e3c439b0ed9a762fca840a0312cd36
brunosana <brunosa@dcomp.ufs.br>
2020-10-01 23:21:57 -0300

    Responsive menu side bar

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AiFillYoutube, AiOutlineHome, AiOutlinePlayCircle, AiOutlineBars,
+  AiFillYoutube, AiOutlineHome, AiOutlinePlayCircle, AiOutlineBars, AiOutlineCloseCircle,
 } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
 import { Nav } from './styles';
 
-const Header: React.FC = () => (
-  <Nav>
-    <button type="button" id="menuSideBar">
-      <AiOutlineBars size={25} />
-    </button>
-    <div id="menuIconBar">
-      <Link to="/">
-        <AiOutlineHome size={16} />
-        {' '}
-        Home
-      </Link>
-      <Link to="/playlists">
-        <AiOutlinePlayCircle size={16} />
-        {' '}
-        Playlist
-      </Link>
-    </div>
-    <AiFillYoutube id="logo" size={34} />
-    <div id="menuFormBar">
-      <input placeholder="Search video..." />
-      <button type="button">Search</button>
-    </div>
-  </Nav>
-);
+const Header: React.FC = () => {
+  const [showSidebar, setShowSideBar] = useState(true);
+
+  function handleShowSideBar() {
+    setShowSideBar(!showSidebar);
+  }
+
+  return (
+    <Nav showSidebar={showSidebar}>
+      <button type="button" id="menuSideBar" onClick={handleShowSideBar}>
+        {showSidebar
+          ? <AiOutlineCloseCircle size={25} />
+          : <AiOutlineBars size={25} />}
+      </button>
+      <div id="menuSide">
+        <Link onClick={handleShowSideBar} to="/">
+          Home
+        </Link>
+        <Link onClick={handleShowSideBar} to="/playlists">
+          Playlist
+        </Link>
+        <div id="menuSideFormBar">
+          <input placeholder="Search video..." />
+          <button type="button">Search</button>
+        </div>
+      </div>
+      <div id="menuIconBar">
+        <Link to="/">
+          <AiOutlineHome size={16} />
+          {' '}
+          Home
+        </Link>
+        <Link to="/playlists">
+          <AiOutlinePlayCircle size={16} />
+          {' '}
+          Playlist
+        </Link>
+      </div>
+      <AiFillYoutube id="logo" size={34} />
+      <div id="menuFormBar">
+        <input placeholder="Search video..." />
+        <button type="button">Search</button>
+      </div>
+    </Nav>
+  );
+};
 
 export default Header;
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Nav = styled.nav`
+interface menuSideBarProps{
+    showSidebar: boolean;
+}
+
+export const Nav = styled.nav<menuSideBarProps>`
     display: flex;
     justify-content: space-between;
     max-width: 1000px;
@@ -15,6 +19,27 @@ export const Nav = styled.nav`
         height: 100%;
         align-items: center;
         margin: 0;
+    }
+
+
+
+    @keyframes slidein {
+        0% {
+            transform: translateX(-45vw);
+        }
+
+        100% {
+            transform: translateX(0);
+        }
+    }
+    @keyframes slideout {
+        0% {
+            transform: translateX(0);
+        }
+
+        100% {
+            transform: translateX(-45vw);
+        }
     }
 
     div{
@@ -81,6 +106,65 @@ export const Nav = styled.nav`
 
     }
 
+    #menuSide{
+        height: 100vh;
+        width: 45vw;
+        display: none;
+        animation-duration: 0.4s;
+        animation-name: slidein;
+        position: absolute;
+        background: #FF0000;
+        margin: -15px;
+        padding-top: 20vw;
+        align-items: center;
+        justify-content: start;
+        z-index: 1;
+        ${(props) => (props.showSidebar
+            && css`
+            display: flex;
+            flex-direction: column;
+            justify-content: start;
+            a{
+                font-size: 25px;
+                & + a { margin-left: 0}
+                &:hover{ color: #282828; svg{ color: #282828; } }
+            }
+            `)}
+    }
+
+    #menuSideFormBar{
+        display: flex;
+        flex-direction: column;
+        input{
+            margin-top: 20px;
+            border: 0;
+            border-radius: 12px;
+            padding: 0 12px;
+            font-size: 14px;
+            height: 35px;
+            width: 90%;
+        }
+        button {
+        border: 0;
+        border-radius: 12px;
+        background: #FFF;
+        margin-top: 20px;
+        margin-left: 0;
+        width: 90%;
+        height: 35px;
+        font-size: 14px;
+        color: #252525;
+        padding: 0 12px;
+        transition: color 0.2s, background 0.2s;
+
+        &:hover {
+            cursor: pointer;
+            color: #FFF;
+            background: #FF0000;
+        }
+        }
+    }
+
     @media (max-width: 555px){
         justify-content: start;
         #menuIconBar{
@@ -91,6 +175,13 @@ export const Nav = styled.nav`
         }
         #menuSideBar{
             display: flex;
+            z-index: 2;
+            ${(props) => props.showSidebar
+            && css`
+             &:hover{
+                svg{color: #282828;}
+            }`}
+
         }
     }
 
