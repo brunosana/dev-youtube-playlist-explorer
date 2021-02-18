174e97bec46279c254682db2644d5c5be1b59885
brunosana <brunosa@dcomp.ufs.br>
2021-02-18 00:25:04 -0300

    build

---
+++ build/precache-manifest.47e850b3b4fdcc4154954e2f95ecaff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4490898430459486ab2e679037c346de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c200575f4b927b15e9",
+    "url": "/static/js/2.1b309017.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/static/js/2.1b309017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fd87b98472d33a492f",
+    "url": "/static/js/main.984852c4.chunk.js"
+  },
+  {
+    "revision": "e88ab2adae80cb41fed1",
+    "url": "/static/js/runtime-main.6a43f089.js"
+  }
+]);
